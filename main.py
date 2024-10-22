70dff6217b28d079c09fa28e9413ac9bff4fdfca
syedhassani <syedmhassani12@gmail.com>
2024-10-22 20:14:43 +1100

    added set activty

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-mytupple=("tan","red","blue" ,"green" )
-print(mytupple[0])
+# mytupple=("tan","red","blue" ,"green" )
+# print(mytupple[0])
 
-mytuple=("red","yellow","blue","green",5,6,8,9)
-mytuple=("red","yellow",[5,6,8,9])
-print(mytuple[2][2])
+# mytuple=("red","yellow","blue","green",5,6,8,9)
+# mytuple=("red","yellow",[5,6,8,9])
+# print(mytuple[2][2])
+# activty
+myset={1,2,3,4,4,4,5,5,6,7,7,7,7,7,7}
+print(myset)
+myset.add(778)
+print(myset)
